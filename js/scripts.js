8ee0d75318e21d7a82eef21a5c97086493755b49
David Zulaica <zulaica@gmail.com>
2015-02-13 12:20:35 -0800

    Get it all to work with HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,38 @@ var triangulator = function(x,y,z) {
 
   return triangle;
 };
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    $("#triangle").hide();
+    $("#error").hide();
+
+    var x = parseInt($("#x_value").val());
+    var y = parseInt($("#y_value").val());
+    var z = parseInt($("#z_value").val());
+
+    var result = triangulator(x,y,z);
+
+    if (result === "nope") {
+      $("#error p").text("Sorry, buddy, but at least one of those lengths is too short to make a triangle. Try again.");
+      $("#error").show()
+    } else if (result === "impossible") {
+      $("#error p").text("Are you for serious?! You can't make a triangle with any values less than or equal to zero. That's unpossible in the third dimension.");
+      $("#error").show()
+    } else {
+      $("#x").text(x);
+      $("#y").text(y);
+      $("#z").text(z);
+      $("#type").text(result);
+      if (result === "scalene") {
+        $("#article").text("a");
+      } else {
+        $("#article").text("an");
+      }
+      $("#triangle").show()
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
